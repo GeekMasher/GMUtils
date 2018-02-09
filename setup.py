89e9b72aacb82fb26039a891d87b6fd50375c367
GeekMasher <geekmasher@gmail.com>
2018-02-09 18:08:20 +0000

    0.0.2 - Added addition files - Added setup.py file - Added __version__.py file

---
+++ setup.py
@@ -0,0 +1,57 @@
+#!/bin/use/python
+
+from os.path import join, dirname, abspath
+from setuptools import setup, find_packages
+
+__HERE__ = abspath(dirname(__file__))
+__SRC__ = join(__HERE__, 'src')
+
+def read(file_path):
+    ret_value ={}
+    with open(file_path, 'r') as file_reader:
+        exec(file_reader.read(), ret_value)
+    return ret_value
+
+
+ABOUT = read(join(__SRC__, 'gmutils', '__version__.py'))
+
+
+setup(
+    name=ABOUT['__name__'],
+    fullname=ABOUT['__name__'] + '.' + ABOUT['__version__'],
+    version=ABOUT['__version__'],
+
+    author=ABOUT['__author__'],
+    author_email=ABOUT['__email__'],
+
+    license=ABOUT['__license__'],
+
+    description=ABOUT['__description__'],
+    long_description=ABOUT['__summary__'],
+
+    url=ABOUT['__url__'],
+    project_urls = {
+        'Source Code': 'https://github.com/GeekMasher/GMUtils',
+        'Bug Tracker': 'https://github.com/GeekMasher/GMUtils/issues'
+    },
+
+    packages=find_packages(__SRC__),
+    package_dir={'':'src'},
+
+    package_data={
+        '': ['*.json'],
+        'data': ['*.json'],
+    },
+    exclude_package_data={
+        '': ['README.md']
+    },
+    include_package_data=True,
+
+    scripts = {
+
+    },
+
+    classifiers=[
+
+    ]
+)
---
+++ src/gmutils/__version__.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+__version__ = "0.1.0"
+
+__title__ = "GMUtils"
+__name__ = "gmutils"
+
+__description__ = "GeekMasher's Open Source Utilities Python Library"
+__summary__ = """\
+GeekMasher created this open source Python utilities library to make his and
+other peoples life easier when writing Python code.
+"""
+
+__url__ = "https://github.com/GeekMasher/GMUtils"
+
+__author__ = "GeekMasher"
+__email__ = "geekmasher@gmail.com"
+
+__maintainer__ = "GeekMasher"
+__mEmail__ = "geekmasher@gmail.com"
+
+__contributors__ = [
+    "GeekMasher <geekmasher@gmail.com>"
+]
+
+__license__ = "MIT License"
+__copyright__ = "Copyright (c) 2018, GeekMasher"
+
+__banner__ = """
+   ____ __  __ _   _ _   _ _
+  / ___|  \/  | | | | |_(_) |___
+ | |  _| |\/| | | | | __| | / __|
+ | |_| | |  | | |_| | |_| | \__ \\
+  \____|_|  |_|\___/ \__|_|_|___/    {}
+
+    {}
+""".format("v"+__version__, __description__)
+
+__all__ = [
+    "__author__", "__copyright__", "__email__",
+    "__license__", "__summary__", "__title__",
+    "__uri__", "__version__", "__banner__",
+    "__contributors__"
+]
